e2f6b8a024f297e69f5ba92ea806fa80c9c9265d
Andy <Doe@Andys-MacBook-Pro.local>
2016-02-01 10:47:45 -0500

    Added files for SuperCool

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var boomAppOutlet: UIImageView!
+    
+    @IBOutlet var bgOutlet: UIImageView!
+    
+    @IBOutlet var buttonOutlet: UIButton!
+    
+    @IBAction func buttonClicked(sender: AnyObject) {
+        
+        buttonOutlet.hidden = true
+        boomAppOutlet.hidden = false
+        bgOutlet.hidden = false
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //boomAppOutlet.hidden = true
+        //bgOutlet.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
